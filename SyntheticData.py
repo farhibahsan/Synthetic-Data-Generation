1d3a8e8e12f05f87f409e9295244ce986ed6036f
farhibahsan <farhib.ahsan@gmail.com>
2021-10-30 21:28:20 -0400

    Update SyntheticData.py

--- SyntheticData.py
+++ SyntheticData.py
@@ -2,24 +2,28 @@ import numpy as np
 import scipy as sp
 import random
 
-graphon_vertical_divider = 0.5
-graphon_horizontal_divider = 0.5
-graphon_p = 0.2
-graphon_q = 0.8
-
-def generateData(max_alternatives):
+def generateData(max_alternatives, beta_a, beta_b, beta_c_d, beta_e, graphon_vertical_boundary=0.5, graphon_horizontal_boundary=0.5, graphon_p=0.2, graphon_q=0.8, debug=False):
 
     route_label_dict = {}
-    num_alternatives = random.randint(2, max_alternatives)
+    num_alternatives = random.randint(2, max(max_alternatives, 2))
+    if debug:
+        print("Num alternatives: %d", num_alternatives)
     route_features = np.zeros((num_alternatives, 3))
     correlation_matrix = np.zeros((num_alternatives, num_alternatives))
-    truth_values = np.zeros((num_alternatives, 1))
+    truth_values = np.zeros((num_alternatives))
 
     for i in range(num_alternatives):
-        a_rand = random.uniform()
-        b_rand = random.uniform()
-        c_rand = random.uniform()
-        route_label = random.uniform()
+        a_rand = random.uniform(0, 1)
+        b_rand = random.uniform(0, 1)
+        c_rand = random.uniform(0, 1)
+        route_label = random.uniform(0, 1)
+        if debug:
+            print("====================================================")
+            print("Iteration", i)
+            print("Value of a:", a_rand)
+            print("Value of b:", b_rand)
+            print("Value of c:", c_rand)
+            print("Value of route label:", route_label)
 
         route_features[i] = np.array([a_rand, b_rand, c_rand])
         route_label_dict.update({i:route_label})
@@ -30,37 +34,38 @@ def generateData(max_alternatives):
         for j in range(i):
             corr_route_label = route_label_dict.get(j)
             
-            if ((route_label < graphon_vertical_divider and corr_route_label < graphon_horizontal_divider) 
-                or (route_label >= graphon_vertical_divider and corr_route_label >= graphon_horizontal_divider)):
+            if ((route_label < graphon_vertical_boundary and corr_route_label < graphon_horizontal_boundary) 
+                or (route_label >= graphon_vertical_boundary and corr_route_label >= graphon_horizontal_boundary)):
                 
                 correlation_matrix[i][j] = graphon_p
+                correlation_matrix[j][i] = graphon_p
 
-                b_eq = 5 + (3 * graphon_p + random.uniform()
-                    + 3 * (1 - graphon_p) + random.uniform()) + random.uniform()
-                e_eq = (graphon_p + random.uniform()
-                    + (1 - graphon_p) + random.uniform()) + random.uniform()
+                b_eq = 5 + (3 * graphon_p + random.uniform(0, 1)
+                    + 3 * (1 - graphon_p) + random.uniform(0, 1)) + random.uniform(0, 1)
+                e_eq = (graphon_p + random.uniform(0, 1)
+                    + (1 - graphon_p) + random.uniform(0, 1)) + random.uniform(0, 1)
             else:
                 correlation_matrix[i][j] = graphon_q
+                correlation_matrix[j][i] = graphon_q
 
-                b_eq = 5 + (3 * graphon_p + random.uniform() 
-                    + 3 * (graphon_q - graphon_p) + random.uniform()
-                    + 3 * (1 - graphon_q) + random.uniform()) + random.uniform()
-                e_eq = (graphon_p + random.uniform()
-                    + (graphon_q - graphon_p) + random.uniform()
-                    + (1 - graphon_q)) + random.uniform() 
+                b_eq = 5 + (3 * graphon_p + random.uniform(0, 1) 
+                    + 3 * (graphon_q - graphon_p) + random.uniform(0, 1)
+                    + 3 * (1 - graphon_q) + random.uniform(0, 1)) + random.uniform(0, 1)
+                e_eq = (graphon_p + random.uniform(0, 1)
+                    + (graphon_q - graphon_p) + random.uniform(0, 1)
+                    + (1 - graphon_q)) + random.uniform(0, 1) 
 
-        h_eq = random.uniform()
-        g_eq = random.uniform()
-        d_eq = 2 * h_eq + random.uniform() 
-        c_eq = g_eq + random.uniform()
-        a_eq = random.uniform()
-        beta_a = random.uniform()
-        beta_b = random.uniform()
-        beta_c_d = random.uniform()
-        beta_e = random.uniform()
+        h_eq = random.uniform(0, 1)
+        g_eq = random.uniform(0, 1)
+        d_eq = 2 * h_eq + random.uniform(0, 1) 
+        c_eq = g_eq + random.uniform(0, 1)
+        a_eq = random.uniform(0, 1)
         v_eq = beta_a * a_eq + beta_b * b_eq + beta_c_d * c_eq * d_eq + beta_e * e_eq
-        u_eq = v_eq + random.uniform()
+        u_eq = v_eq + random.uniform(0, 1)
         truth_values[i] = u_eq
+
+        # if debug:
+
 
     prob_y = np.exp(truth_values) / np.sum(np.exp(truth_values))
     index_y = np.random.choice(num_alternatives, p=prob_y)
@@ -68,3 +73,13 @@ def generateData(max_alternatives):
     y[index_y] = 1
 
     return route_features, correlation_matrix, y
+
+def main():
+    route_features, corr, y = generateData(5, 1, 1, 1, 1, debug=True)
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print(route_features)
+    print(corr)
+    print(y)
+
+if __name__ == "__main__":
+    main()
